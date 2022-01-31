278572369782b5e2a9b878dadf703f1bf4b17d81
Helder Henrique <helder.h.silva@grad.ufsc.br>
2022-01-31 11:01:13 -0300

    Add T3

---
+++ Trabalhos/Unidade_4/AVL/MinhaListaEncadeada.hpp
@@ -0,0 +1,342 @@
+#ifndef MINHALISTAENCADEADA_H
+#define MINHALISTAENCADEADA_H
+
+#include "ListaEncadeadaAbstrata.hpp"
+
+template <typename T>
+class MinhaListaEncadeada :  public ListaEncadeadaAbstrata<T>
+{
+        public:
+    MinhaListaEncadeada()
+    {
+        this->_tamanho = 0;
+        this->_primeiro = nullptr;
+    };
+
+    ~MinhaListaEncadeada()
+    {
+
+        Elemento<T> *EL_Atual = this->_primeiro;
+
+        while (EL_Atual != nullptr)
+        {
+            Elemento<T> *EL_Auxiliar = EL_Atual->_proximo;
+
+            delete EL_Atual;
+
+            EL_Atual = EL_Auxiliar;
+        }
+    };
+
+    /*-*/
+
+    /**
+     * @brief Indica se há algum item na lista ou não.
+     *
+     * @return true se houver algum item na lista; false caso contrário.
+     */
+    bool estaVazia()
+    {
+        if (this->getTamanho() == 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    };
+
+    /**
+     * @brief Obtém a quantidade de itens na lista.
+     *
+     * @return Um inteiro maior ou igual a 0.
+     */
+    int getTamanho()
+    {
+        return this->_tamanho;
+    };
+
+    /**
+     * @brief Obtém a posição de um item na lista. Lança
+     * ExcecaoListaEncadeadaVazia caso a lista esteja vazia ou
+     * ExcecaoDadoInexistente caso o item não esteja contido na lista.
+     *
+     * @param dado O item cuja posição deseja-se obter.
+     * @return Um inteiro na faixa [0, tamanho); se houver mais que um mesmo
+     * item na lista, a posição da primeira ocorrência.
+     */
+    int posicao(T umDado)
+    {
+        if (this->estaVazia())
+        {
+            throw lista_encadeada_vazia_exception();
+        }
+
+        Elemento<T> *EL_Atual = this->_primeiro;
+        int index = 0;
+
+        while (EL_Atual != nullptr)
+        {
+            if (EL_Atual->_dado == umDado)
+            {
+                return index;
+            }
+            index++;
+            EL_Atual = EL_Atual->_proximo;
+        }
+        throw posicao_invalida_exception();
+    };
+
+    /**
+     *
+     * @brief Indica se um dado item está contido na lista ou não.
+     *
+     * @param dado O item sendo buscado.
+     * @return true se o item está contido na lista; false caso contrário.
+     */
+    bool contem(T umDado)
+    {
+        Elemento<T> *EL_Atual = this->_primeiro;
+
+        while (EL_Atual != nullptr)
+        {
+            if (EL_Atual->_dado == umDado)
+            {
+                return true;
+            }
+            EL_Atual = EL_Atual->_proximo;
+        }
+        return false;
+    };
+
+    /**
+     * @brief Insere um item no início da lista.
+     *
+     * @param dado O item sendo inserido.
+     */
+    void adicionaNoInicio(T umDado)
+    {
+        Elemento<T> *EL_InserirInicio = new Elemento<T>();
+
+        EL_InserirInicio->_dado = umDado;
+        EL_InserirInicio->_proximo = this->_primeiro;
+
+        this->_primeiro = EL_InserirInicio;
+        this->_tamanho++;
+        return;
+    };
+
+    /**
+     * @brief Insere um item em uma posição específica da lista. Lança
+     * ExcecaoPosicaoInvalida caso a posição não esteja na faixa
+     * [0, tamanho].
+     *
+     * @param posicao Um inteiro dentro da faixa [0, tamanho]. Ao se inserir em
+     * uma posição já ocupada, a posição do item que já estava naquela posição
+     * será posicao + 1; inserir-se na posicao tamanho significa inserir-se no
+     * fim da lista.
+     * @param dado O item sendo inserido.
+     */
+    void adicionaNaPosicao(T umDado, int umaPosicao)
+    {
+        if (umaPosicao < 0 || umaPosicao >= this->_tamanho + 1)
+        {
+            throw posicao_invalida_exception();
+        }
+
+        if (umaPosicao == 0)
+        {
+            this->adicionaNoInicio(umDado);
+            return;
+        }
+
+        if (umaPosicao == this->_tamanho)
+        {
+            this->adicionaNoFim(umDado);
+            return;
+        }
+
+        Elemento<T> *EL_Auxiliar = this->_primeiro;
+
+        for (int i = 1; i < umaPosicao; i++)
+        {
+            EL_Auxiliar = EL_Auxiliar->_proximo;
+        }
+
+        Elemento<T> *EL_InserirMeio = new Elemento<T>();
+
+        EL_InserirMeio->_dado = umDado;
+        EL_InserirMeio->_proximo = EL_Auxiliar->_proximo;
+
+        EL_Auxiliar->_proximo = EL_InserirMeio;
+        this->_tamanho++;
+        return;
+    };
+
+    /**
+     * @brief Insere um item no fim da lista.
+     *
+     * @param dado O item sendo inserido.
+     */
+    void adicionaNoFim(T umDado)
+    {
+        if (this->_primeiro == nullptr)
+        {
+            this->adicionaNoInicio(umDado);
+            return;
+        }
+
+        Elemento<T> *EL_Auxiliar = this->_primeiro;
+
+        while (EL_Auxiliar->_proximo != nullptr)
+        {
+            EL_Auxiliar = EL_Auxiliar->_proximo;
+        }
+
+        Elemento<T> *EL_InserirFim = new Elemento<T>();
+        EL_InserirFim->_dado = umDado;
+        EL_InserirFim->_proximo = nullptr;
+
+        EL_Auxiliar->_proximo = EL_InserirFim;
+        this->_tamanho++;
+
+        return;
+    };
+
+    /**
+     * @brief Remove o primeiro item da lista. Lança ExcecaoListaEncadeadaVazia
+     * caso não haja nenhum item na lista.
+     *
+     * @return O item removido.
+     */
+    T retiraDoInicio()
+    {
+        if (this->estaVazia())
+        {
+            throw lista_encadeada_vazia_exception();
+        }
+
+        Elemento<T> *EL_RemoverInicio = this->_primeiro;
+        this->_primeiro = EL_RemoverInicio->_proximo;
+        T Auxiliar = EL_RemoverInicio->_dado;
+
+        delete EL_RemoverInicio;
+
+        this->_tamanho--;
+
+        return Auxiliar;
+    };
+
+    /**
+     * @brief Remove um item de uma posição específica da lista. Lança
+     * ExcecaoPosicaoInvalida caso a posição não esteja na faixa [0, tamanho).
+     *
+     * @param posicao Um inteiro dentro da faixa [0, tamanho).
+     * @return O item removido.
+     */
+    T retiraDaPosicao(int umaPosicao)
+    {
+        if (umaPosicao < 0 || umaPosicao >= this->_tamanho)
+        {
+            throw posicao_invalida_exception();
+        }
+
+        if (umaPosicao == 0)
+        {
+            return this->retiraDoInicio();
+        }
+
+        if (umaPosicao == this->_tamanho - 1)
+        {
+            return this->retiraDoFim();
+        }
+
+        Elemento<T> *EL_Auxiliar = this->_primeiro;
+
+        for (int i = 0; i < umaPosicao - 1; i++)
+        {
+            EL_Auxiliar = EL_Auxiliar->_proximo;
+        }
+
+        Elemento<T> *EL_Remover = this->_primeiro;
+
+        for (int j = 0; j < umaPosicao; j++)
+        {
+            EL_Remover = EL_Remover->_proximo;
+        }
+
+        T Auxiliar = EL_Remover->_dado;
+        EL_Auxiliar->_proximo = EL_Remover->_proximo;
+
+        delete EL_Remover;
+
+        this->_tamanho--;
+
+        return Auxiliar;
+    };
+
+    /**
+     * @brief Remove o último item da lista. Lança ExcecaoListaEncadeadaVazia
+     * caso não haja nenhum item na lista.
+     *
+     * @return O item removido.
+     */
+    T retiraDoFim()
+    {
+        if (this->estaVazia())
+        {
+            throw lista_encadeada_vazia_exception();
+        }
+
+        if (this->_tamanho == 1)
+        {
+            return this->retiraDoInicio();
+        }
+
+        Elemento<T> *EL_RemoverFim = this->_primeiro;
+        Elemento<T> *EL_AntesFim = this->_primeiro;
+
+        while (EL_RemoverFim->_proximo != nullptr)
+        {
+            EL_RemoverFim = EL_RemoverFim->_proximo;
+        }
+
+        while (EL_AntesFim->_proximo != EL_RemoverFim)
+        {
+            EL_AntesFim = EL_AntesFim->_proximo;
+        }
+
+        EL_AntesFim->_proximo = nullptr;
+        T Auxiliar = EL_RemoverFim->_dado;
+
+        delete EL_RemoverFim;
+
+        this->_tamanho--;
+
+        return Auxiliar;
+    };
+
+    /**
+     * @brief Remove um item específico da lista. Lança
+     * ExcecaoListaEncadeadaVazia caso não haja nenhum item na lista ou
+     * ExcecaoDadoInexistente caso o item não esteja contido na lista.
+     *
+     * @param dado O item a ser removido. Se houver mais que um item com
+     * o mesmo valor, remove a primeira ocorrência.
+     */
+    T retiraEspecifico(T umDado)
+    {
+        if (this->getTamanho() == 0)
+        {
+            throw lista_encadeada_vazia_exception();
+        }
+
+        if (this->contem(umDado))
+        {
+            return this->retiraDaPosicao(this->posicao(umDado));
+        }
+
+        throw posicao_invalida_exception();
+    };
+};
+
+#endif
---
+++ Trabalhos/Unidade_4/ArvoreBinaria/MinhaArvoreDeBuscaBinaria.hpp
@@ -0,0 +1,454 @@
+#ifndef MINHAARVOREDEBUSCABINARIA_HPP
+#define MINHAARVOREDEBUSCABINARIA_HPP
+
+#include "ArvoreDeBuscaBinaria.hpp"
+#include <cassert>
+#include <utility>
+
+/**
+ * @brief Representa uma árvore binária de busca.
+ * 
+ * @tparam T O tipo de dado guardado na árvore.
+ */
+template<typename T>
+class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
+{
+    protected:
+    int buscaQuantidade(Nodo<T> *chave) const
+    {
+        if (chave != nullptr)
+        {
+            return (1 + this->buscaQuantidade(chave->filhoEsquerda) + this->buscaQuantidade(chave->filhoDireita));
+        }
+        return 0;
+    }
+
+    /***********************************************************************************************************/
+
+    void percursoEmOrdem(Nodo<T> *chave, ListaEncadeadaAbstrata<T> *listaEmOrdem) const
+    {
+        if (chave != nullptr)
+        {
+            this->percursoEmOrdem(chave->filhoEsquerda, listaEmOrdem);
+            listaEmOrdem->adicionaNoFim(chave->chave);
+            this->percursoEmOrdem(chave->filhoDireita, listaEmOrdem);
+        }
+        return;
+    }
+
+    void percursoPreOrdem(Nodo<T> *chave, ListaEncadeadaAbstrata<T> *listaPreOrdem) const
+    {
+        if (chave != nullptr)
+        {
+            listaPreOrdem->adicionaNoFim(chave->chave);
+            this->percursoPreOrdem(chave->filhoEsquerda, listaPreOrdem);
+            this->percursoPreOrdem(chave->filhoDireita, listaPreOrdem);
+        }
+        return;
+    }
+
+    void percursoPosOrdem(Nodo<T> *chave, ListaEncadeadaAbstrata<T> *listaPosOrdem) const
+    {
+        if (chave != nullptr)
+        {
+            this->percursoPosOrdem(chave->filhoEsquerda, listaPosOrdem);
+            this->percursoPosOrdem(chave->filhoDireita, listaPosOrdem);
+            listaPosOrdem->adicionaNoFim(chave->chave);
+        }
+        return;
+    }
+
+    /***********************************************************************************************************/
+
+    void insereNaArvore(Nodo<T> *chaveAtual, T novoValor)
+    {
+        if (this->vazia())
+        {
+            Nodo<T> *novaChave = new Nodo<T>{novoValor, 0};
+
+            delete this->_raiz;
+
+            this->_raiz = novaChave;
+
+            return;
+        }
+
+        if (novoValor < chaveAtual->chave)
+        {
+            if (chaveAtual->filhoEsquerda == nullptr)
+            {
+                Nodo<T> *novaChave = new Nodo<T>{novoValor, chaveAtual->altura+1};
+
+                chaveAtual->filhoEsquerda = novaChave;
+            }
+            else
+            {
+                this->insereNaArvore(chaveAtual->filhoEsquerda, novoValor);
+            }
+        }
+        else
+        {
+            if (chaveAtual->filhoDireita == nullptr)
+            {
+                Nodo<T> *novaChave = new Nodo<T>{novoValor, chaveAtual->altura+1};
+
+                chaveAtual->filhoDireita = novaChave;
+            }
+            else
+            {
+                this->insereNaArvore(chaveAtual->filhoDireita, novoValor);
+            }
+        }
+        return;
+    }
+
+    /***********************************************************************************************************/
+
+    Nodo<T> *buscaContem(T chave, Nodo<T> *raiz) const
+    {
+        while (raiz != nullptr && raiz->chave != chave)
+        {
+            if (raiz->chave < chave)
+            {
+                raiz = raiz->filhoDireita;
+            }
+            else
+            {
+                raiz = raiz->filhoEsquerda;
+            }
+        }
+        return raiz; 
+    }
+
+    Nodo<T> *buscaPai(T chave, Nodo<T> *pai) const
+    {
+        while (pai != nullptr && pai->chave != chave && pai->filhoEsquerda->chave != chave && pai->filhoDireita->chave != chave)
+        {
+            if (pai->chave < chave)
+            {
+                pai = pai->filhoDireita;
+            }
+            else
+            {
+                pai = pai->filhoEsquerda;
+            }
+        }
+        return pai;
+    }
+
+    /***********************************************************************************************************/
+
+    int alturaSubArvore(Nodo<T> *chave) const
+    {
+        int alturaEsquerda = 0, alturaDireita = 0, altura = 0;
+
+        if (chave->filhoEsquerda != nullptr)
+        {
+            alturaEsquerda = 1 + alturaSubArvore(chave->filhoEsquerda);
+        }
+
+        if (chave->filhoDireita != nullptr)
+        {
+            alturaDireita = 1 + alturaSubArvore(chave->filhoDireita);
+        }
+
+        return checaAltura(alturaEsquerda, alturaDireita, altura);
+    }
+
+    int checaAltura(int Esquerda, int Direita, int altura) const
+    {
+        if (Esquerda >= Direita)
+        {
+            altura = Esquerda;
+            return altura;
+        }
+        else
+        {
+            altura = Direita;
+            return altura;
+        }
+    }
+
+    /***********************************************************************************************************/
+
+    T removeDaArvore(T chaveParaRemover, Nodo<T> *chavePai)
+    {
+        if (chavePai == nullptr)
+        {
+            return chavePai->chave;
+        }
+
+        Nodo<T> *chave;
+
+        if (chavePai->chave == chaveParaRemover)
+        {
+            if (verificaFolha(chavePai))
+            {
+                T chaveRemovida = chavePai->chave;
+
+                chavePai = nullptr;
+                delete chavePai;
+
+                return chaveRemovida;
+            }
+            chave = chavePai;
+        }
+        else if (chavePai->filhoEsquerda != nullptr && chavePai->filhoEsquerda->chave == chaveParaRemover)
+        {
+            chave = chavePai->filhoEsquerda;
+
+            if (verificaFolha(chave))
+            {
+                T chaveRemovida = chave->chave;
+
+                chavePai->filhoEsquerda = nullptr;
+                delete chave;
+
+                return chaveRemovida;
+            }
+        }
+        else if (chavePai->filhoDireita != nullptr && chavePai->filhoDireita->chave == chaveParaRemover)
+        {
+            chave = chavePai->filhoDireita;
+
+            if (verificaFolha(chave))
+            {
+                T chaveRemovida = chave->chave;
+
+                chavePai->filhoDireita = nullptr;
+                delete chave;
+
+                return chaveRemovida;
+            }
+        }
+        
+        if(chave != nullptr && chave->filhoDireita != nullptr)
+        {
+            Nodo<T> *chaveAux = chave->filhoDireita;
+            Nodo<T> *chavePaiAux = chave;
+
+            while (chaveAux->filhoEsquerda != nullptr)
+            {
+                chavePaiAux = chaveAux;
+                chaveAux = chaveAux->filhoEsquerda;
+            }
+
+            chave->chave = chaveAux->chave;
+            chaveAux->chave = chaveParaRemover;
+            return this->removeDaArvore(chaveParaRemover, chavePaiAux);
+        }
+        return chaveParaRemover;
+    }
+
+    bool verificaFolha(Nodo<T> *chave)
+    {
+        if (chave->filhoEsquerda == nullptr && chave->filhoDireita == nullptr)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /***********************************************************************************************************/
+
+    void destruirArvore(Nodo<T> *chave)
+    {
+        if (chave != nullptr)
+        {
+            this->destruirArvore(chave->filhoEsquerda);
+            this->destruirArvore(chave->filhoDireita);
+            delete chave;
+        }
+    }
+    
+    public:
+    ~MinhaArvoreDeBuscaBinaria() 
+    {
+        if (this->_raiz != nullptr)
+        {
+        this->destruirArvore(this->_raiz);
+        }
+        return;
+    };
+
+    /**
+     * @brief Verifica se a arvore esta vazia
+     * @return Verdade se a arvore esta vazia.
+     */
+    bool vazia() const 
+    {
+        if (this->quantidade() == 0 || this->_raiz == nullptr)
+        {
+            return true;
+        }
+        return false;
+    };
+    
+    /**
+     * @brief Retornar quantidade de chaves na arvore
+     * @return Numero natural que representa a quantidade de chaves na arvore
+     */
+    int quantidade() const 
+    {
+        int quantidade = 0;
+
+        quantidade = this->buscaQuantidade(this->_raiz);
+
+        return quantidade;
+    };
+    
+    /**
+     * @brief Verifica se a arvore contem uma chave
+     * @param chave chave a ser procurada na arvore
+     * @return Verdade se a arvore contem a chave
+     */
+    bool contem(T chave) const 
+    {    
+        if (!this->vazia())
+        {
+            Nodo<T> *raiz = this->buscaContem(chave, this->_raiz);
+
+            if (raiz == nullptr)
+            {
+                return false;
+            }
+
+            if (raiz->chave == chave)
+            {
+                return true;
+            }
+
+        }
+        return false;
+    };
+    
+    /**
+     * @brief Retorna a altura da (sub)arvore
+     * @param chave chave que é raiz da (sub)arvore cuja altura queremos. Se chave é nula, retorna a altura da arvore.
+     * @return Numero inteiro representando a altura da (subarvore). Se chave nao esta na arvore, retorna std::nullopt
+     */
+    std::optional<int> altura(T chave) const 
+    {
+        if (this->contem(chave))
+        {
+            Nodo<T> *raiz = this->buscaContem(chave, this->_raiz);
+            return this->alturaSubArvore(raiz);
+        }
+        return std::nullopt;
+    };
+
+    /**
+     * @brief Insere uma chave na arvore
+     * @param chave chave a ser inserida
+     */        
+    void inserir(T chave) 
+    {
+        return this->insereNaArvore(this->_raiz, chave);
+    };
+
+    /**
+     * @brief Remove uma chave da arvore
+     * @param chave chave a removida
+     * @return Retorna a chave removida ou nullptr se a chave nao esta na arvore
+     */        
+    void remover(T chave)
+    {
+        Nodo<T> *raiz = this->_raiz;
+
+
+        if(raiz->chave == chave && this->verificaFolha(raiz))
+        {
+            this->_raiz = nullptr;
+
+            delete raiz;
+
+            return;
+        }
+
+        if (this->contem(chave))
+        {
+            Nodo<T> *chavePai = this->buscaPai(chave, raiz);
+            this->removeDaArvore(chave, chavePai);
+        }
+        return;
+    };
+
+    /**
+     * @brief Busca a chave do filho a esquerda de uma (sub)arvore
+     * @param chave chave da arvore que eh chaveP do filho a esquerda
+     * @return Chave do filho a esquerda. Se chave nao esta na arvore, retorna std::nullopt
+     */
+    std::optional<T> filhoEsquerdaDe(T chave) const 
+    {
+        if (this->contem(chave))
+        {
+            Nodo<T> *chavePai = this->buscaContem(chave, this->_raiz);
+
+            if (chavePai->filhoEsquerda != nullptr)
+            {
+                return chavePai->filhoEsquerda->chave;
+            }
+        }
+        return std::nullopt;
+    };
+
+    /**
+     * @brief Busca a chave do filho a direita de uma (sub)arvore
+     * @param chave chave da arvore que eh pai do filho a direita
+     * @return Chave do filho a direita. Se chave nao esta na arvore, retorna nullptr
+     */        
+    std::optional<T> filhoDireitaDe(T chave) const 
+    {
+        if (this->contem(chave))
+        {
+            Nodo<T> *chavePai = this->buscaContem(chave, this->_raiz);
+
+            if (chavePai->filhoDireita != nullptr)
+            {
+                return chavePai->filhoDireita->chave;
+            }
+        }
+        return std::nullopt;
+    };
+
+    /**
+     * @brief Lista chaves visitando a arvore em ordem
+     * @return Lista encadeada contendo as chaves em ordem. 
+     */
+    ListaEncadeadaAbstrata<T>* emOrdem() const 
+    {
+        ListaEncadeadaAbstrata<T> *listaEmOrdem = new MinhaListaEncadeada<T>();
+
+        this->percursoEmOrdem(this->_raiz, listaEmOrdem);
+
+        return listaEmOrdem;
+    };
+
+    /**
+     * @brief Lista chaves visitando a arvore em pre-ordem
+     * @return Lista encadeada contendo as chaves em pre-ordem. 
+     */
+    ListaEncadeadaAbstrata<T>* preOrdem() const 
+    {
+        ListaEncadeadaAbstrata<T> *listaPreOrdem = new MinhaListaEncadeada<T>();
+
+        this->percursoPreOrdem(this->_raiz, listaPreOrdem);
+
+        return listaPreOrdem;
+    };
+
+    /**
+     * @brief Lista chaves visitando a arvore em pos-ordem
+     * @return Lista encadeada contendo as chaves em pos ordem. 
+     */
+    ListaEncadeadaAbstrata<T>* posOrdem() const 
+    {
+        ListaEncadeadaAbstrata<T> *listaPosOrdem = new MinhaListaEncadeada<T>();
+
+        this->percursoPosOrdem(this->_raiz, listaPosOrdem);
+
+        return listaPosOrdem;
+    };
+};
+
+#endif
